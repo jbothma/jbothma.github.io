fee45206fa653142dcfdcbf7a84323834508f4f2
JD Bothma <jbothma@gmail.com>
2025-09-18 22:39:08 +0100

    Static build to out dir

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const path = require('path')
 
 const nextConfig: NextConfig = {
-  turbopack: { root: path.join(__dirname, '..') }
+  turbopack: { root: path.join(__dirname, '..') },
+  output: "export",
 };
 
 export default nextConfig;
