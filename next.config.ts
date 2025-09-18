68541c3644c210f03bd6d0603e7f8b56b09f8437
JD Bothma <jbothma@gmail.com>
2025-09-18 22:37:06 +0100

    Initial next.js

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const path = require('path')
+
+const nextConfig: NextConfig = {
+  turbopack: { root: path.join(__dirname, '..') }
+};
+
+export default nextConfig;
